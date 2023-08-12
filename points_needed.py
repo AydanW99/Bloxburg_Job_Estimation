d0d34c4847799108117a2cfecb36d6a384960fe9
AydanW99 <aidanwilliams62@gmail.com>
2023-08-12 16:00:42 +0100

    Formatting and completed both TODOs
    - Works for each Job
    
     - User can select the level they want to reach

--- points_needed.py
+++ points_needed.py
@@ -1,5 +1,3 @@
-# TODO Works for each job dictionary made be needed
-# TODO have the user choose the highest lvl with a default of 50
 
 
 class PointsNeeded():
@@ -9,11 +7,14 @@ class PointsNeeded():
         self.previous_level = 0
         self.new_points_needed = 0
         self.total_points = 0
-        self.average_time = 12
+        self.average_time = 0
+        self.highest_level = 0
 
     def user_stats(self):
+        """This is where the user imports their stats in order for the formula to calculate"""
         self.current_level = int(input("What is your current level? "))
-        for x in range(1, self.current_level):
+        self.highest_level = int(input("What level do you want to reach? "))
+        for _ in range(1, self.current_level):
             self.new_points_needed = self.current_points_needed + self.previous_level * 2 + 1
             self.current_points_needed = self.new_points_needed
             self.previous_level += 1
@@ -26,7 +27,7 @@ class PointsNeeded():
         self.formula()
 
     def formula(self):
-        for x in range(self.current_level, 50):
+        for _ in range(self.current_level, self.highest_level):
             self.new_points_needed = self.current_points_needed + self.previous_level * 2 + 1
             self.total_points += self.new_points_needed
             self.current_points_needed = self.new_points_needed
