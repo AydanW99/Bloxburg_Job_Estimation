6b869f5f31fb30a1060696a80146fe5ab263e7ce
AydanW99 <aidanwilliams62@gmail.com>
2023-08-13 11:47:07 +0100

    formatting

--- bloxburg.py
+++ bloxburg.py
@@ -1,5 +1,42 @@
-from points_needed import PointsNeeded
+DEFAULT_VALUE = 15
 
-points_needed = PointsNeeded()
+class Bloxburg():
+    def __init__(self):
+        self.current_level = 0
+        self.current_points_needed = 11
+        self.previous_level = 0
+        self.new_points_needed = 0
+        self.total_points = 0
+        self.average_time = 0
+        self.highest_level = 0
 
-points_needed.user_stats()
+    def enter_stats(self):
+        """This is where the user imports their stats in order for the formula to calculate"""
+        self.current_level = int(input("What is your current level? "))
+        self.highest_level = int(input("What level do you want to reach? "))
+        for _ in range(1, self.current_level):
+            self.new_points_needed = self.current_points_needed + self.previous_level * 2 + 1
+            self.current_points_needed = self.new_points_needed
+            self.previous_level += 1
+        user_input_time = input("What is the average minutes to reach 50 points, "
+                                      "leave empty to use standard value of 15? ")
+        if int(user_input_time) > 0:
+            self.average_time = int(user_input_time)
+        else:
+            self.average_time = DEFAULT_VALUE
+        self.time_required()
+
+    def time_required(self):
+        for _ in range(self.current_level, self.highest_level):
+            self.new_points_needed = self.current_points_needed + self.previous_level * 2 + 1
+            self.total_points += self.new_points_needed
+            self.current_points_needed = self.new_points_needed
+            self.previous_level += 1
+
+        average_1 = self.average_time / 50
+
+        time_required = average_1 * self.total_points
+
+        print(f"\nYou have {self.total_points} points left to obtain")
+        print(f"From your current level it will take you {round(time_required)} mins to get to Level 50 \n"
+              f"which is {round(time_required / 60)} hours")
---
+++ main.py
@@ -0,0 +1,5 @@
+from bloxburg import Bloxburg
+
+bloxburg = Bloxburg()
+
+bloxburg.enter_stats()
